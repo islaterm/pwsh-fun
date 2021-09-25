54c36d42196afddd3968bebd08b40d0484468652
SL8R <islaterm@gmail.com>
2021-09-25 13:30:42 -0300

    ConvertTo-MP3

---
+++ ConvertTo-Mp3.ps1
@@ -0,0 +1,39 @@
+function ConvertTo-MP3 {
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory=$true)]
+      [string]
+      $Path,
+      [Alias("C")]
+      [switch]
+      $Cleanup
+  )
+  $originalLocation = Get-Location
+  try {
+    Set-Location $Path
+    foreach ($file in $(Get-ChildItem -Exclude *.mp3)) {
+      if ($Cleanup) {
+        $out = $(ffmpeg.exe -y -i $file.Name -vn -ar 44100 -ac 2 -b:a 320k "$($file.BaseName).mp3") 2>&1
+        Write-Host $out
+        if ($out -match 'Output') {
+          Remove-Item $file -Verbose
+        }
+      } else {
+        ffmpeg.exe -y -i $file.Name -vn -ar 44100 -ac 2 -b:a 320k "$($file.BaseName).mp3"
+      }
+    }
+  } finally {
+    Set-Location $originalLocation
+  }
+  <#
+    .SYNOPSIS
+      Converts a list of files to mp3.
+    .DESCRIPTION
+      Makes a conversion of all valid audio files of a directory to .mp3 extension using ffmpeg.
+    .PARAMETER path
+      The location of the audio files.
+    .PARAMETER cleanup
+      If present, source files will be removed after conversion (fot this, the scripts needs to 
+      capture ffmpeg's console output).
+  #>
+}
