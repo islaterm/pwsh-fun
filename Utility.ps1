8d083e75a4d05ca160b0deb91f39430bca236882
R8V <islaterm@gmail.com>
2022-05-09 17:01:51 -0400

    Implemented function to set a remote ssh key

--- Utility.ps1
+++ Utility.ps1
@@ -1,21 +1,3 @@
-<#
-.SYNOPSIS
-  Checks if a command exists.
-.INPUTS
-  System.String. The name of the command to check.
-.OUTPUTS
-  <Bool>. Test-Command returns '$true' if the command exists, 
-  '$false' otherwise
-.EXAMPLE
-  PS> Test-Command winget
-  True
-.EXAMPLE
-  PS> Test-Command julia
-  False
-.EXAMPLE
-  PS> 'julia' | Test-Command
-  False
-#>
 function Test-Command {
   param (
     # The name of the command to check.
@@ -33,25 +15,26 @@ function Test-Command {
   } finally {
     $ErrorActionPreference = $originalErrorAction
   }
+  <#
+  .SYNOPSIS
+    Checks if a command exists.
+  .INPUTS
+    System.String. The name of the command to check.
+  .OUTPUTS
+    <Bool>. Test-Command returns '$true' if the command exists, 
+    '$false' otherwise
+  .EXAMPLE
+    PS> Test-Command winget
+    True
+  .EXAMPLE
+    PS> Test-Command julia
+    False
+  .EXAMPLE
+    PS> 'julia' | Test-Command
+    False
+  #>
 }
 
-<#
-.SYNOPSIS
-  Checks if an application is installed on the system.
-.OUTPUTS
-  System.Boolean. Returns '$true' if the application is installed, 
-  '$false' otherwise
-.EXAMPLE
-  PS> Test-Application 'NVIDIA Web Helper.exe'
-  # True if the service NVIDIA Web Helper is registered, False otherwise
-.EXAMPLE
-  PS> Test-Application -Name '7-zip'
-  # True if 7-zip is installed, false otherwise
-.EXAMPLE
-  PS> Test-Application -DisplayName '3Tene'
-  # True if 3Tene is installed, regardless of the installation method
-  # (If using Steam this could have been registered with a 'Name' like Steam App XXXXXX)
-#>
 function Test-Application {
   [CmdletBinding(DefaultParameterSetName = 'Name')]
   param (
@@ -75,4 +58,21 @@ function Test-Application {
           Where-Object { $_.DisplayName -eq $DisplayName })
   }
   $process.Length -ne 0
+  <#
+  .SYNOPSIS
+    Checks if an application is installed on the system.
+  .OUTPUTS
+    System.Boolean. Returns '$true' if the application is installed, 
+    '$false' otherwise
+  .EXAMPLE
+    PS> Test-Application 'NVIDIA Web Helper.exe'
+    # True if the service NVIDIA Web Helper is registered, False otherwise
+  .EXAMPLE
+    PS> Test-Application -Name '7-zip'
+    # True if 7-zip is installed, false otherwise
+  .EXAMPLE
+    PS> Test-Application -DisplayName '3Tene'
+    # True if 3Tene is installed, regardless of the installation method
+    # (If using Steam this could have been registered with a 'Name' like Steam App XXXXXX)
+  #>
 }
