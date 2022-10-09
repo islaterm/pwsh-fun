afe90ed93c090f1a1d5bc7392a7a5cb64534da2c
R8V <ignacio.slater@ug.uchile.cl>
2022-10-08 21:18:04 -0300

    (WIP)FEAT Declare Get-PSModulesPath function

---
+++ Ravenhill.Console/1.1/Get-PSModulesPath.ps1
@@ -0,0 +1,29 @@
+function Get-PSModulesPath {
+  [CmdletBinding()]
+  param (
+    
+  )
+  
+  begin {
+    
+  }
+  
+  process {
+    
+  }
+  
+  end {
+    
+  }
+  <#
+  .SYNOPSIS
+    Gets the path to the PowerShell modules directory.
+  .INPUTS
+    None.
+  .OUTPUTS
+    System.String. The path to the PowerShell modules directory.
+  .EXAMPLE
+    PS> Get-PSModulesPath
+    C:\Users\R8V\Documents\WindowsPowerShell\Modules
+  #>
+}
