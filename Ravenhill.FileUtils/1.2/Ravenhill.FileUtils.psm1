fdfbb0b0a7c3d058f2c3e8fac10385a2d8efcd08
R8V <ignacio.slater@ug.uchile.cl>
2023-05-08 09:57:59 -0400

    FEAT Partially implements compress directories

--- Ravenhill.FileUtils/1.2/Ravenhill.FileUtils.psm1
+++ Ravenhill.FileUtils/1.2/Ravenhill.FileUtils.psm1
@@ -1,6 +1,7 @@
 . $PSScriptRoot\Ravenhill.FileUtils_aux.ps1
 . $PSScriptRoot\Video\ConvertTo-H265.ps1
 . $PSScriptRoot\Remove-FilesByExtension.ps1
+. $PSScriptRoot\Compress-Directories.ps1
 
 function Remove-EmptyDirectories {
   param (
@@ -41,67 +42,4 @@ function Remove-EmptyDirectories {
     https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
   #>
 }
-
-$BaseExtension = @{
-  'cbz' = 'zip';
-  'cbr' = 'rar';
-  'cbt' = 'tar';
-  'cb7' = '7z'  
-}
-
-function Compress-Directories {
-  [Alias('cmdir')]
-  param (
-    # The directory containing the subdirectories to compress.
-    [Parameter(Mandatory,
-      Position = 0,
-      ValueFromPipeline,
-      ValueFromPipelineByPropertyName = $true,
-      HelpMessage = "Path to one or more locations.")]
-    [ValidateNotNullOrEmpty()]
-    [SupportsWildcards()]
-    [string[]]
-    $Path,
-    # The format to compress the directory to.
-    [Alias('f')]
-    [Parameter(Mandatory = $true)]
-    [ValidateSet('zip', 'rar', 'tar', '7z', 'bz2', 'gz', 'xz', 'cbz', 'cbr', 'cbt', 'cb7')]
-    [string]
-    $Format
-  )
-  $isVerbose = $PSBoundParameters.ContainsKey('Verbose') ? $Verbose : $false
-  $isDebug = $PSBoundParameters.ContainsKey('Debug') ? $Debug : $false
-  Write-Verbose "Compressing directories in $Path to $Format"
-  $Path = $PSBoundParameters.ContainsKey('Path') ? $Path : $(Get-Location)
-  Write-Debug "Resolved path: $Path"
-  $Extension = $BaseExtension.ContainsKey($Format) ? $BaseExtension[$Format] : $Format
-  Write-Debug "Resolved extension: $Extension"
-  Get-ChildItem -Path $Path -Directory | ForEach-Object {
-    sz a "$($_.FullName).$Extension" $_.FullName 
-  }
-  if (-not ($Extension -eq $Format)) {
-    Get-ChildItem -Path $Path -Filter *.$Extension | `
-      Move-Item -Destination { [System.IO.Path]::ChangeExtension($_.FullName, $Format) } -Force `
-      -Verbose:$isVerbose -Debug:$isDebug
-  }
-  Get-ChildItem -Path $Path -Directory | Remove-Item -Recurse -Force -Verbose:$isVerbose `
-    -Debug:$isDebug
-  <#
-    .SYNOPSIS
-      Compress all subdirectories of a directory into a given format.
-    .DESCRIPTION
-      Compress all subdirectories using 7zip.
-      This function accepts all valid 7zip formats, along with some comic book formats (cbz, cbr, 
-      cb7).
-      After the compression is done, the original directories are removed.
-      If no path is given, the current directory is used.
-    .EXAMPLE
-      Compress-Directories -Path "C:\Users\User\Documents\My Documents" -Format "zip"
-    .EXAMPLE
-      'D:\Manga\Yoshihiro Togashi\Hunter X Hunter - 1998\' | Compress-Directories -Format cb7
-    .EXAMPLE
-      Compress-Directories -Format 7zip
-  #>
-}
-New-Alias -Name Compress-Directories -Value cmdir
 
